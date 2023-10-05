0f29f9ff26534e86edc13e35065a3122fb405364
Your Name <mehdi.boussaid.free@gmail.com>
2023-10-05 06:11:57 +0200

    des test

--- src/pages/secretCollection.tsx
+++ src/pages/secretCollection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import HeaderConnected from "@/components/headerConnected";
 import Carousel from "@/components/carousel";
+// import TestCarousel from "@/components/testCarousel";
 
 const SecretCollection: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const SecretCollection: React.FC = () => {
 
       <main>
         <HeaderConnected />
+        {/* <TestCarousel></TestCarousel> */}
         <Carousel></Carousel>
       </main>
     </>
