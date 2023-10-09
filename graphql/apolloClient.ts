33cfb3f6834bc28672003c930acfa9586e7bad7b
Your Name <mehdi.boussaid.free@gmail.com>
2023-10-09 07:23:47 +0200

    cors local and prod

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -2,9 +2,13 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 // !!! éviter cela en prod !!!!
 // use ENV instead
-
+// uri: "http://localhost:3000/api/graphql",
+// uri: "https://main.ds8lkanha4tdu.amplifyapp.com/api/graphql",
 const client = new ApolloClient({
-  uri: "https://main.ds8lkanha4tdu.amplifyapp.com/api/graphql",
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://main.ds8lkanha4tdu.amplifyapp.com/api/graphql"
+      : "http://localhost:3000/api/graphql",
   cache: new InMemoryCache(),
 });
 
