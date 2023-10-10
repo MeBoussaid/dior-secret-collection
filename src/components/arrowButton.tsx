b2b0581fcdf58b35b1bd37dcc6b2b9b5307ff59e
Your Name <mehdi.boussaid.free@gmail.com>
2023-10-10 05:36:43 +0200

    correcytions and cleaning

--- src/components/arrowButton.tsx
+++ src/components/arrowButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../styles/arrowButton.module.scss";
 
 interface arrowButtonProps {
@@ -7,16 +7,6 @@ interface arrowButtonProps {
 }
 
 const ArrowButton: React.FC<arrowButtonProps> = ({ direction, onClick }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
   const handleClick = () => {
     onClick();
   };
@@ -29,11 +19,8 @@ const ArrowButton: React.FC<arrowButtonProps> = ({ direction, onClick }) => {
         height="90"
         viewBox="0 0 69 139"
         fill="none"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        className={isHovered ? styles.hovered : ""}
       >
-        <g opacity={isHovered ? "0.34" : "1"}>
+        <g opacity="1">
           {direction === "previous" && (
             <path
               d="M68.1221 2.99539C68.6719 2.37565 68.6151 1.4276 67.9954 0.877859C67.3756 0.328121 66.4276 0.384869 65.8779 1.00461L68.1221 2.99539ZM3 74.1496L1.87786 73.1543L0.931721 74.2209L1.949 75.2199L3 74.1496ZM65.8779 1.00461L1.87786 73.1543L4.12214 75.145L68.1221 2.99539L65.8779 1.00461ZM1.949 75.2199L65.949 138.07L68.051 135.93L4.051 73.0794L1.949 75.2199Z"
